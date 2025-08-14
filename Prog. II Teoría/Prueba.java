6446896623ace58d23c2267e4b0ad0109c364855
Fabri98 <fabricioalegre9828@gmail.com>
2025-08-14 20:20:25 -0300

    Create Prueba.java

---
+++ Prog. II Teoría/Prueba.java
@@ -0,0 +1,9 @@
+Public class Prueba {
+    public static void main(String[] args) {
+        Programa p = new Programa();
+        p.nombre = "Firulais";
+        p.edad = 3;
+        p.especie = "perro";
+        p.habla();
+    }
+}
