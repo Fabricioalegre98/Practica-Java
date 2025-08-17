b7958b3df8eac647fe4c81ff3771dce6529a8c6b
Fabri98 <fabricioalegre9828@gmail.com>
2025-08-17 17:33:36 -0300

    .

---
+++ Prog. II Práctica/14-08-25/index.java
@@ -0,0 +1,13 @@
+public class index { // clase principal
+
+    public static void main(String[] args) 
+        {
+            System.out.println("Hello, World!");
+        }
+
+        public static void greet() 
+        {
+            System.out.println("Welcome to the Java Course!");
+        }
+
+}
