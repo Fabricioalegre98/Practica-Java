e63c3045e6af40a1751c9b7f3b6a7ed35fdb41ac
Fabri98 <fabricioalegre9828@gmail.com>
2025-08-21 19:07:43 -0300

    .

--- Prog. II Práctica/21-08-25/supTriangulo.java
+++ Prog. II Práctica/21-08-25/supTriangulo.java
@@ -1,11 +1,20 @@
 import java.util.Scanner; // Importamos la clase Scanner para leer la entrada del usuario
 
 public class supTriangulo {
+    private static int A= 10;
+    private static final int B =20;
+
     public static void main(String[]args) {
       System.out.println("tu nombre: ");
       Scanner Input = new Scanner(System.in);
         String Nombre = Input.nextLine(); // Leemos el nombre del usuario
       System.out.println(Nombre);
-      
+
+        // Ejemplo de suma de dos números
+
+
+        System.out.println(A+B);
+        A = A + B; // Suma de A y B
+
     }
 }
