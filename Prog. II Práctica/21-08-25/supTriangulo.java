c69dfbfb538b1a5c2b91f4af439983657c8fff53
Fabri98 <fabricioalegre9828@gmail.com>
2025-08-21 18:50:19 -0300

    Create supTriangulo.java

---
+++ Prog. II Práctica/21-08-25/supTriangulo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; // Importamos la clase Scanner para leer la entrada del usuario
+
+public class supTriangulo {
+    public static void main(String[]args) {
+      System.out.println("tu nombre: ");
+      Scanner Input = new Scanner(System.in);
+        String Nombre = Input.nextLine(); // Leemos el nombre del usuario
+      System.out.println(Nombre);
+      
+    }
+}
