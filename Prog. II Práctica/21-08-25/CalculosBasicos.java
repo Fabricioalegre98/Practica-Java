5eef1b6ba8869328ef967571f751f5d57668ccf6
Fabri98 <fabricioalegre9828@gmail.com>
2025-08-21 19:59:49 -0300

    Update CalculosBasicos.java

--- Prog. II Práctica/21-08-25/CalculosBasicos.java
+++ Prog. II Práctica/21-08-25/CalculosBasicos.java
@@ -1,20 +1,20 @@
 public class CalculosBasicos {
     public static void main(String[]args){
-        int A,B, C;
+        int A,B;
         java.util.Scanner sc = new java.util.Scanner(System.in);
         System.out.println("Ingrese el valor de A: ");
         A = sc.nextByte();
         System.out.println("Ingrese el valor de B: ");
         B = sc.nextByte();
         if (A>B) {
-            C = A * B;
-            System.out.println("El resultado es: " + C);
+            A = A * B;
+            System.out.println("El resultado es: " + A);
         }else if (A<B) {
-            C = B /A;
-            System.out.println("El resultado es: " + C);
+            A = B /A;
+            System.out.println("El resultado es: " + A);
         }else{
-            C = A + B;
-            System.out.println("El resultado es: " + C);
+            A = A + B;
+            System.out.println("El resultado es: " + A);
         }
     }
 }
